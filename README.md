eb31d3a8d24674d6e4b2f70a0679996c31db60ac
“mzubhan <msubhan2812@gmail.com>
2023-04-07 19:44:55 +0700

    Fix reviewed codes

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 proyek dikerjakan menggunakan html, css, dan javascript
 
+proyek ini adalah proyek galeri di mana pengguna dapat menambahkan foto ke dalam koleksi mereka, pengguna juga dapat mengubah nama serta pekerjaan mereka.
+
 link: https://mzubhan.github.io/web_project_4_id/index.html
--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,30 @@
 let openPopup = document.querySelector('.profile__edit-button');
+let closePopup = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_information');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__information');
+
 function showDisplay(){
    document.querySelector('.popup_opened').style.display = 'flex';
+
+   nameInput.value = profileName.textContent
+   jobInput.value = profileJob.textContent
 }
 
-let closePopup = document.querySelector('.popup__close-button');
 function closeDisplay() {
    document.querySelector('.popup_opened').style.display = 'none';
 }
 
-let formElement = document.querySelector('.popup__form');
 function submitText(evt) {
    evt.preventDefault();
-
-   let nameInput = document.querySelector('.popup__input_name');
-   let jobInput = document.querySelector('.popup__input_information');
-   let profileName = document.querySelector('.profile__name');
-   let profileJob = document.querySelector('.profile__information');
 
    profileName.textContent = nameInput.value
    profileJob.textContent = jobInput.value
 
-   nameInput.placeholder = profileName.textContent;
-   jobInput.placeholder = profileJob.textContent;
-
-   let popupDisplay = document.querySelector('.popup_opened');
-   popupDisplay.style.display = 'none';
-
-   nameInput.value = "";
-   jobInput.value = "";
+   closeDisplay()
 }
 
 openPopup.addEventListener('click', showDisplay);
