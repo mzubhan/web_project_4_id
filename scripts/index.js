dcc7127b1b23d5e150ac61203b82e8bcebffe751
“mzubhan <msubhan2812@gmail.com>
2023-04-22 04:11:37 +0700

    Fix reviewed codes

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,76 @@
 //EDIT PROFILE
-let popupEditProfile = document.querySelector('.edit-profile');
-let editProfileButton = document.querySelector('.profile__edit-button');
-let closeEditPofile = popupEditProfile.querySelector('.popup__close-button');
-let popupEditProfileContainer = popupEditProfile.querySelector('.popup');
+const popupEditProfile = document.querySelector('.edit-profile');
+const editProfileButton = document.querySelector('.profile__edit-button');
+const closeEditPofile = popupEditProfile.querySelector('.popup__close-button');
+const popupEditProfileContainer = popupEditProfile.querySelector('.popup');
 
 //ADD PHOTO
-let popupAddPhoto = document.querySelector('.add-photo');
-let addPhotoButton = document.querySelector('.profile__add-button');
-let closeAddPhoto = popupAddPhoto.querySelector('.popup__close-button');
-let popupAddPhotoContainer = popupAddPhoto.querySelector('.popup');
-let submitPhoto = popupAddPhoto.querySelector('.popup__form');
-let photoTitle = popupAddPhoto.querySelector('.popup__input_judul');
-let photoUrl = popupAddPhoto.querySelector('.popup__input_tautan-gambar');
+const popupAddPhoto = document.querySelector('.add-photo');
+const addPhotoButton = document.querySelector('.profile__add-button');
+const closeAddPhoto = popupAddPhoto.querySelector('.popup__close-button');
+const popupAddPhotoContainer = popupAddPhoto.querySelector('.popup');
+const submitPhoto = popupAddPhoto.querySelector('.popup__form');
+const photoTitle = popupAddPhoto.querySelector('.popup__input_judul');
+const photoUrl = popupAddPhoto.querySelector('.popup__input_tautan-gambar');
 
 //FORM
-let formElement = popupEditProfile.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_information');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__information');
+const formElement = popupEditProfile.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_information');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__information');
 
 //PHOTO GRID
-let photoGrid = document.querySelector('.grid');
-let photoCard = photoGrid.querySelector('.grid__photo-card');
-let deletePhotoButton = photoGrid.querySelector('.grid__delete-button');
-let lovePhotoButton = photoGrid.querySelector('.grid__love-button');
-let imageGrid = photoGrid.querySelector('.grid__photo');
-let lovePhotoButtonActive = photoGrid.querySelector('.grid__love-button_active');
+const photoGrid = document.querySelector('.grid');
+const photoCard = photoGrid.querySelector('.grid__photo-card');
+const deletePhotoButton = photoGrid.querySelector('.grid__delete-button');
+const lovePhotoButton = photoGrid.querySelector('.grid__love-button');
+const imageGrid = photoGrid.querySelector('.grid__photo');
+const lovePhotoButtonActive = photoGrid.querySelector('.grid__love-button_active');
 
 //POPUP PHOTO GRID
-let popupImage = document.querySelector('.image-popup');
-let popupImageContainer = popupImage.querySelector('.image-popup__container');
-let closePopupImage = popupImage.querySelector('.image-popup__delete-btn');
+const popupImage = document.querySelector('.image-popup');
+const popupImageContainer = popupImage.querySelector('.image-popup__container');
+const closePopupImage = popupImage.querySelector('.image-popup__delete-btn');
+
+//PHOTO ARRAY
+const initialCards = [
+   {
+     name: "Lembah Yosemite",
+     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+   },
+   {
+     name: "Danau Louise",
+     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+   },
+   {
+     name: "Pegunungan Gundul",
+     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+   },
+   {
+     name: "Gunung Latemar",
+     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+   },
+   {
+     name: "Taman Nasional Vanoise",
+     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+   },
+   {
+     name: "Lago di Braies",
+     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+   }
+ ];
+
+ initialCards.forEach((card) => {
+   const addPhotoCard = document.querySelector('.add-photo-card').content;
+   const addPhotoElement = addPhotoCard.querySelector('.grid__photo-card').cloneNode(true);
+
+   addPhotoElement.querySelector('.grid__title').textContent = card.name;
+   addPhotoElement.querySelector('.grid__photo').alt = card.name; 
+   addPhotoElement.querySelector('.grid__photo').src = card.link; 
+
+   photoGrid.prepend(addPhotoElement);
+ });
 
 //POPUP EDIT PROFILE
 function showDisplay(){
@@ -65,13 +104,14 @@ function closePhotoSection() {
 }
 
 function addPhoto(titleValue, urlValue){
-   let addPhotoCard = document.querySelector('.add-photo-card').content;
-   let addPhotoElement = addPhotoCard.querySelector('.grid__photo-card').cloneNode(true);
+   const addPhotoCard = document.querySelector('.add-photo-card').content;
+   const addPhotoElement = addPhotoCard.querySelector('.grid__photo-card').cloneNode(true);
 
    addPhotoElement.querySelector('.grid__title').textContent = titleValue;
    addPhotoElement.querySelector('.grid__photo').src = urlValue;  
+   addPhotoElement.querySelector('.grid__photo').alt = titleValue;
 
-   photoGrid.appendChild(addPhotoElement);   
+   photoGrid.prependChild(addPhotoElement);   
 }
 
 //SUBMIT PHOTO
@@ -86,26 +126,38 @@ function addPhoto(titleValue, urlValue){
    closePhotoSection()
 });
 
-// DELETE, LIKE, AND POPUP PHOTO
+//DELETE PHOTO
 function deletePhoto(evt) {
    const target = evt.target
    if (target.classList.contains('grid__delete-button')) {
-      target.parentElement.remove()
-   } else if (target.classList.contains('grid__love-button')) {
-      target.classList.toggle('grid__love-button_active')
-   } else if (target.classList.contains('grid__photo')) {
+   target.parentElement.remove()
+}};
 
-      let title = target.closest('.grid__title');
-      let source = target.closest('.grid__photo');
 
-      popupImage.classList.add('image-popup_opened')
+//LIKE PHOTO
+function likePhoto(evt) {
+   const target = evt.target
+   if (target.classList.contains('grid__love-button')) {
+   target.classList.toggle('grid__love-button_active'
+   )
+}};
 
-      popupImageContainer.querySelector('.image-popup__title').textContent = target.parentElement.querySelector('.grid__title').textContent
-      popupImageContainer.querySelector('.image-popup__photo').src = target.parentElement.querySelector('.grid__photo').src
-      popupImageContainer.querySelector('.image-popup__photo').alt = target.parentElement.querySelector('.grid__photo').alt
+//POPUP PHOTO
+function popupPhoto(evt){
+   const target = evt.target
+   if (target.classList.contains('grid__photo')) {
 
-   }
- }
+   const title = target.closest('.grid__title');
+   const source = target.closest('.grid__photo');
+
+   popupImage.classList.add('image-popup_opened')
+
+   popupImageContainer.querySelector('.image-popup__title').textContent = target.parentElement.querySelector('.grid__title').textContent
+
+
+   popupImageContainer.querySelector('.image-popup__photo').src = target.parentElement.querySelector('.grid__photo').src
+   popupImageContainer.querySelector('.image-popup__photo').alt = target.parentElement.querySelector('.grid__photo').alt
+}}
 
 //FOR EDIT PROFILE
 editProfileButton.addEventListener('click', showDisplay);
@@ -117,18 +169,20 @@ addPhotoButton.addEventListener('click', openPhotoSection);
 closeAddPhoto.addEventListener('click', closePhotoSection);
 
 //DELETE AND LIKE PHOTO
-photoGrid.addEventListener('click', deletePhoto)
+photoGrid.addEventListener('click', deletePhoto);
+photoGrid.addEventListener('click', likePhoto);
+photoGrid.addEventListener('click', popupPhoto)
 
 //CLOSE IMAGE POPUP
 closePopupImage.addEventListener('click', function() {
       popupImage.classList.remove('image-popup_opened')
 })
 
-photoCard.addEventListener('click', function(event) {
-   if (event.target.className === 'grid__delete-button') {
-      photoGrid.removeChild('.grid__photo-card')
-   }
- });
+// photoCard.addEventListener('click', function(event) {
+//    if (event.target.className === 'grid__delete-button') {
+//       photoGrid.removeChild('.grid__photo-card')
+//    }
+//  });
 
 
 // **CODE DUMP**
@@ -162,3 +216,24 @@ photoCard.addEventListener('click', function(event) {
 //    })
 // })
 
+
+// DELETE, LIKE, AND POPUP PHOTO
+// function deletePhoto(evt) {
+//    const target = evt.target
+//    if (target.classList.contains('grid__delete-button')) {
+//       target.parentElement.remove()
+//    } else if (target.classList.contains('grid__love-button')) {
+//       target.classList.toggle('grid__love-button_active')
+//    } else if (target.classList.contains('grid__photo')) {
+
+//       const title = target.closest('.grid__title');
+//       const source = target.closest('.grid__photo');
+
+//       popupImage.classList.add('image-popup_opened')
+
+//       popupImageContainer.querySelector('.image-popup__title').textContent = target.parentElement.querySelector('.grid__title').textContent
+//       popupImageContainer.querySelector('.image-popup__photo').src = target.parentElement.querySelector('.grid__photo').src
+//       popupImageContainer.querySelector('.image-popup__photo').alt = target.parentElement.querySelector('.grid__photo').alt
+
+//    }
+//  }
